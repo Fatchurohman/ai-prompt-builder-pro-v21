29a9c85477f618f727fdfbcf3a5d005bf02e8236
Fatchurohman <fatur62@gmail.com>
2026-08-17 16:54:11 +0700

    Add BPM options to the select element
    Populate the BPM select element with predefined values.

--- js/bpm.js
+++ js/bpm.js
@@ -1,1 +1,25 @@
+const bpmList = [
+    60,
+    70,
+    80,
+    90,
+    100,
+    110,
+    120,
+    128,
+    140,
+    160
+];
+const bpmSelect = document.getElementById("bpm");
 
+bpmList.forEach(item => {
+
+    const option = document.createElement("option");
+
+    option.value = item;
+
+    option.textContent = item;
+
+    bpmSelect.appendChild(option);
+
+});
