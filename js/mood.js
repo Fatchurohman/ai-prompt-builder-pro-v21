04ae6d6d52903389258ab0163efd32e61cd644a7
Fatchurohman <fatur62@gmail.com>
2026-08-17 16:56:30 +0700

    Add mood list and populate select options
    Added a list of moods and populated a select element with options.

--- js/mood.js
+++ js/mood.js
@@ -1,1 +1,37 @@
-coba
+const moodList=[
+
+"Select Mood",
+
+"Happy",
+"Sad",
+"Romantic",
+"Melancholic",
+"Energetic",
+"Powerful",
+"Emotional",
+"Hopeful",
+"Inspirational",
+"Epic",
+"Dark",
+"Mysterious",
+"Dreamy",
+"Relaxing",
+"Peaceful",
+"Spiritual",
+"Uplifting"
+
+];
+
+const moodSelect=document.getElementById("mood");
+
+moodList.forEach(item=>{
+
+const option=document.createElement("option");
+
+option.value=item;
+
+option.textContent=item;
+
+moodSelect.appendChild(option);
+
+});
